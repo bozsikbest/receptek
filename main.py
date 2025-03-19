f6b980971a1c89a574d825d47757c06da8ad3e61
asd <asd>
2025-03-19 19:22:32 +0100

    asd

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('upload'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -40,14 +40,14 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or urlparse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('upload')
         return redirect(next_page)
     return render_template('auth/login.html', form=form)
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('upload'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data)
@@ -61,13 +61,17 @@ def register():
 @auth.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('upload'))
 
 app.register_blueprint(auth, url_prefix='/auth')
 
 @app.route('/')
-@login_required
 def index():
+    return render_template('base.html')
+
+@app.route('/upload')
+@login_required
+def upload():
     try:
         return render_template('index.html', 
                              tinymce_api_key="cic11ch0565sd3xwncfbpuleqz0vw4cdxr3823ok37gddt53")
